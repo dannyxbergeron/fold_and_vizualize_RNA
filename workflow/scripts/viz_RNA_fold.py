8846901654ebf17a4f478a528171265c2691d0df
Danny <danny.bergeron@hotmail.com>
2021-08-19 16:25:41 -0400

    tried to fix a problem with some vizualization, turn out it is a bug in the librairy...

--- workflow/scripts/viz_RNA_fold.py
+++ workflow/scripts/viz_RNA_fold.py
@@ -14,15 +14,40 @@ with open(mfe_file, 'r') as f:
     mfe = mfe.replace('Free Energy of Ensemble', 'mfe').strip()
 
 cg = forgi.load_rna(fx_file, allow_many=False)
+seq_len = len(cg._seq)
+# print(f'--> SEQ_LEN: {seq_len}')
+
+# plot params
+FONTSIZE = 4
+LINEWIDTH_BACKBONE = 0.5
+LINEWIDTH_BASEPAIR = 1
+
+'''
+For some reasons, the forgi is crashing with some structurs.
+In order to make it work for all my structures, I changed the folowing
+line:
+    vec=vec/ftuv.magnitude(vec)
+for this:
+    if ftuv.magnitude(vec) == 0:
+        vec=vec/0.1
+    else:
+        vec=vec/ftuv.magnitude(vec)
+at line 370 of the file:
+workflow/.snakemake/conda/<YOU_CONDA_ENV>/lib/python3.8/site-packages/forgi/visual/mplotlib.py
+
+The issue was reported, but unfortunately never corrected, see:
+https://github.com/ViennaRNA/forgi/issues/39
+'''
+
 ax, _ = fvm.plot_rna(
      cg,
-     text_kwargs={"fontweight":"ultralight", 'fontsize': 4, "color": '#2F3236'},
+     text_kwargs={"fontweight":"ultralight", 'fontsize': FONTSIZE, "color": '#2F3236'},
      lighten=0.85,
      color=True,
-     backbone_kwargs={"linewidth":0.5, "color": "#2F3236"},
-     basepair_kwargs={"linewidth":1, 'color': 'red'},
+     backbone_kwargs={"linewidth":LINEWIDTH_BACKBONE, "color": "#2F3236"},
+     basepair_kwargs={"linewidth":LINEWIDTH_BASEPAIR, 'color': 'red'},
 )
 
 ax.set_title(f'{snakemake.wildcards[0]}\n({mfe})')
-# plt.show()
 plt.savefig(out, format='svg')
+# plt.show()
